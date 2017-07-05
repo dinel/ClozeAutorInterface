a941bd84217a68201618e806bc41d318d6f21704
Constantin Orasan <c.orasan@wlv.ac.uk>
2017-07-05 12:17:42 +0100

    Baseline version which has all the components for displaying.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,37 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
+        if(! $this->container->get('session')->isStarted()) {
+            $session = new Session();
+            $session->start();
+            $session->set("state", "welcome");
+        } else {        
+            $session = $request->getSession();
+        }
+            
+        switch ($session->get("state")) {                
+            case "disclaimer":
+                $session->set("state", "survey");
+                return $this->render('default/disclaimer.html.twig', [ ]);
+                
+            case "survey":
+                $session->set("state", "text");
+                return $this->render('default/survey.html.twig', [ ]);
+                
+            case "text":
+                $session->set("state", "thankyou");
+                $text = explode(" ", "This is a test [string] that will be [displayed] with gaps");
+                return $this->render('default/text.html.twig', [ 
+                    "text" => $text,
+                ]);
+                
+            case "thankyou":
+                $session->invalidate();
+                return $this->render('default/thankyou.html.twig', [ ]);                
+                
+            default:
+                $session->set("state", "disclaimer");
+                return $this->render('default/index.html.twig', [ ]);
+        }        
+    }           
 }
---
+++ web/js/cloze.js
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    document.getElementById(data).classList.remove("filler-in-list");
+    document.getElementById(data).classList.add("filler-in-text");
+    ev.target.classList.remove("gap");
+    ev.target.classList.add("filled-gap");
+    filled_gaps++;
+    
+    if(filled_gaps === no_gaps) {
+        $('#submit-result').removeClass("disabled");
+    }
+}
+
+$( document ).ready(function() {        
+    $('#text').on('click', '.filled-gap', function() {
+        $(this).addClass('gap');
+        $(this).removeClass('filled-gap');
+        $(this).children().last().removeClass('filler-in-text');
+        $(this).children().last().addClass('filler filler-in-list');
+        $('#words').append($(this).children().last());
+        filled_gaps--;
+    
+        if(filled_gaps !== no_gaps) {
+            $('#submit-result').addClass("disabled");
+        }
+    });    
+});
