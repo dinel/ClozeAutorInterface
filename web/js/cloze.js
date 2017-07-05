4bf1f147b98825fdad214160a24f86c3f31fc360
Constantin Orasan <c.orasan@wlv.ac.uk>
2017-07-05 15:50:01 +0100

    Added logging information

--- web/js/cloze.js
+++ web/js/cloze.js
@@ -5,6 +5,7 @@
  */
 
 var saveClicked = false;
+var operations = "";
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -16,10 +17,10 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    document.getElementById(data).classList.remove("filler-in-list");
-    document.getElementById(data).classList.add("filler-in-text");
+    var idWord = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(idWord));
+    document.getElementById(idWord).classList.remove("filler-in-list");
+    document.getElementById(idWord).classList.add("filler-in-text");
     ev.target.classList.remove("gap");
     ev.target.classList.add("filled-gap");
     filled_gaps++;
@@ -27,20 +28,31 @@ function drop(ev) {
     if(filled_gaps === no_gaps) {
         $('#submit-result').removeClass("disabled");
     }
+    
+    idGap = ev.target.id;
+    
+    updateLog("Moving " + idWord + " to gap " + idGap);
 }
 
-$( document ).ready(function() {        
+$( document ).ready(function() {
+    updateLog("Opened text");
+    
     $('#text').on('click', '.filled-gap', function() {
         $(this).addClass('gap');
         $(this).removeClass('filled-gap');
         $(this).children().last().removeClass('filler-in-text');
         $(this).children().last().addClass('filler filler-in-list');
+        idWord = $(this).children().last().attr('id');
         $('#words').append($(this).children().last());
         filled_gaps--;
     
         if(filled_gaps !== no_gaps) {
             $('#submit-result').addClass("disabled");
         }
+                
+        idGap = $(this).attr('id');
+        
+        updateLog("Remove " + idWord + " from " + idGap);
     });
     
     $(window).bind('beforeunload', function(){
@@ -49,7 +61,22 @@ $( document ).ready(function() {
         }
     });
     
+    $(window).blur(function() {
+        updateLog("Changed window");
+    });
+    
+    $(window).focus(function() {
+        updateLog("Switched back");
+    });
+    
     $('#submit-result').click(function() {
         saveClicked = true;
+        updateLog("Submitted solution");
     });
 });
+
+function updateLog(s) {
+    var d = new Date();
+    console.log("At " + d.getHours() + ":" + d.getMinutes() 
+            + ":" + d.getSeconds() + ":" + s);
+}
