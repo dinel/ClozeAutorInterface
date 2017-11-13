185a7d54f438cea4ced5b20bb1e7d0ef45997029
Constantin Orasan <dinel@dinel.org.uk>
2017-11-13 11:32:19 +0000

    Redesigned the interface for anaphoricity cueing

--- web/js/anaphoricity.js
+++ web/js/anaphoricity.js
@@ -7,71 +7,6 @@
 var saveClicked = false;
 var operations = "";
 
-function allowDrop(ev) {
-    
-    if(ev.target.id[0] === "w") {        
-        idGap = ev.target.id.substring(ev.target.id.indexOf("-") + 1, 
-                                       ev.target.id.lastIndexOf("-"));
-    } else {
-        idGap = ev.target.id.substring(3);
-    }
-    
-    refWord = ev.dataTransfer.getData("text").substring(
-                    ev.dataTransfer.getData("text").indexOf("-") + 1, 
-                    ev.dataTransfer.getData("text").lastIndexOf("-"));
-    
-    if(idGap === refWord) {
-        ev.preventDefault();
-    }
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);    
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var idWord = ev.dataTransfer.getData("text");
-    var target = ev.target;
-    
-    // ensure that the gap is not filled already
-    if(! target.classList.contains("gap")) {
-        target.classList.remove('filler-in-text');
-        target.classList.add('filler-in-list');        
-        target.classList.add('filler');
-        target.setAttribute('draggable', 'true');
-        var parent = target.parentNode;
-        $('#words'+parent.id.substring(3)).append(target);
-        target = parent;
-        
-        /*
-        idWord = $(this).children().last().attr('id');
-        idGap = $(this).attr('id');
-        
-        updateLog("R:" + idWord + ":" + idGap);
-        */
-        $("#words"+parent.id.substring(3)).shuffleChildren();
-    }    
-    
-    $('#'+target.id).find('.gap-fillers').removeClass("red-border");
-    
-    // check the parent
-    if(document.getElementById(idWord).parentNode.classList.contains("filled-gap")) {
-        document.getElementById(idWord).parentNode.classList.remove("filled-gap");
-        document.getElementById(idWord).parentNode.classList.add("gap");
-    }
-    
-    target.appendChild(document.getElementById(idWord));
-    document.getElementById(idWord).setAttribute('draggable', 'false');
-    document.getElementById(idWord).classList.remove("filler-in-list");
-    document.getElementById(idWord).classList.add("filler-in-text");
-    target.classList.remove("gap");
-    target.classList.add("filled-gap");   
-    
-    idGap = ev.target.id;
-    
-    updateLog("M:" + idWord + ":" + idGap);
-}
 
 $( document ).ready(function() {
     $(".gap-fillers").each(function() {
@@ -80,17 +15,15 @@ $( document ).ready(function() {
     
     updateLog("Opened text");    
     
-    $('#text').on('click', '.filled-gap', function() {
-        $(this).addClass('gap');
-        $(this).removeClass('filled-gap');
-        $(this).children().last().removeClass('filler-in-text');
-        $(this).children().last().addClass('filler filler-in-list');
-        $(this).children().last().attr('draggable', 'true');
-        idGap = $(this).attr('id');
-        idWord = $(this).children().last().attr('id');
-        $('#words'+idGap.substring(3)).append($(this).children().last());        
-        updateLog("R:" + idWord + ":" + idGap);
-        $("#words").shuffleChildren();
+    $('.filler').click(function() {
+        var select = $.grep(this.className.split(" "), function(v, i){
+                return v.indexOf('filler-') === 0;
+        }).join();
+        console.log(select);
+        $("." + select).removeClass("highlighted");
+        $(this).addClass("highlighted");
+        $(this).parent().removeClass("red-border");
+        $(this).parent().removeClass("not-answered");
     });
     
     $(window).bind('beforeunload', function(){
@@ -108,14 +41,12 @@ $( document ).ready(function() {
     });
     
     $('#submit-result').click(function() {
-        var notAnswered = $('.gap');
+        var notAnswered = $('.not-answered');
         
         if(notAnswered.length === 0) {
             alert("Going to submit the results");
-        } else {
-            notAnswered.each(function() {
-               $(this).find('.gap-fillers').addClass("red-border");
-            });
+        } else {            
+            $('.not-answered').addClass("red-border");
             
             $('#incomplete').show();
             setTimeout(function() {
